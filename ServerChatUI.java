3a6a73ca99ae1591001f38d2da7af5ec3d453bd6
Gabriel <gabriel.d.richard@gmail.com>
2018-12-07 17:07:57 -0500

    Built version 1.0 of runClient()

---
+++ ServerChatUI.java
@@ -0,0 +1,164 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.TitledBorder;
+
+public class ServerChatUI extends JFrame implements Accessible {
+	private static final long serialVersionUID = 1L;
+	
+	JButton send;
+	JTextField message;
+	JButton sendButton;
+	JTextArea display;
+	ObjectOutputStream outputStream;
+	Socket socket;
+	ConnectionWrapper connection;
+
+	public ServerChatUI(Socket socket) {
+		this.socket = socket;
+		setFrame(createUI());
+		runClient();
+	}
+
+	public JPanel createUI() {
+
+		ActionListener controller = new Controller();
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+
+		JPanel messagePane = new JPanel();
+		messagePane.setBorder(
+				BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLACK, 10), "MESSAGE"));
+		messagePane.setLayout(new BorderLayout());
+
+		JPanel nestedMessage = new JPanel();
+		nestedMessage.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		nestedMessage.setLayout(new BorderLayout(5, 5));
+
+		JTextField textMessage = new JTextField("Type message");
+
+		send = new JButton("Send");
+		send.setMnemonic('s');
+		send.setEnabled(true);
+		send.addActionListener(controller);
+		send.setPreferredSize(new Dimension(100, 0));
+		getRootPane().setDefaultButton(send);
+
+		nestedMessage.add(textMessage, BorderLayout.CENTER);
+		nestedMessage.add(send, BorderLayout.EAST);
+
+		messagePane.add(nestedMessage);
+
+		/* chat display panel */
+		JPanel chat = new JPanel();
+		chat.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.BLUE, 10), "CHAT DISPLAY",
+				TitledBorder.CENTER, TitledBorder.CENTER));
+		chat.setLayout(new BorderLayout(5, 5));
+
+		JTextArea textChat = new JTextArea(30, 45);
+		// chat.add(textChat, BorderLayout.CENTER);
+		textChat.setEditable(false);
+
+		JScrollPane scroll = new JScrollPane(textChat);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+		chat.add(scroll);
+		// chat.add(hbar,BorderLayout.SOUTH);
+
+		panel.add(messagePane, BorderLayout.NORTH);
+		panel.add(chat, BorderLayout.CENTER);
+
+		return panel;
+	}
+
+	/**
+	 * Creates input/output streams and starts a new thread.
+	 */
+	private void runClient() {
+		connection = new ConnectionWrapper(socket);
+		try {
+			connection.createStreams();
+			outputStream = connection.getOutputStream();
+			Runnable runnable = new ChatRunnable<ServerChatUI>(ServerChatUI.this, connection);
+			Thread thread = new Thread(runnable);
+			thread.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	final void setFrame(JPanel pane) {
+
+		setMinimumSize(new Dimension(588, 500));
+		setContentPane(pane);
+		addWindowListener(new WindowsController());
+		setResizable(false);
+	}
+
+	private class WindowsController extends WindowAdapter {
+		@Override
+		public void windowClosing(WindowEvent e) {
+			
+			System.out.println("ServerUI Window Closing");
+			dispose();
+			System.exit(0);
+		}
+	}
+
+	private class Controller implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent event) {
+
+			send();
+		}
+
+		private void send() {
+			String sendMessage = message.getText();
+			display.append(sendMessage + "\n");
+
+			/*
+			 * and then uses the outputStream to write the following string object:
+			 * ChatProtocolConstants.DISPLACMENT + sendMessage
+			 * +ChatProtocolConstants.LINE_TERMINATOR
+			 */
+
+		}
+	}
+
+	@Override
+	public JTextArea getDisplay() {
+		return display;
+	}
+
+	@Override
+	public void closeChat() {
+
+		try {
+			socket.close();
+			dispose();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
